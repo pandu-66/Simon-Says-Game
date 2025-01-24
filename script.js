61f87e735664d3a78325eedec54904948c1e3ff9
Ejjineni Panduraj <ejjineniraju@gmail.com>
2025-01-24 19:01:26 +0530

    Updated Levels

--- script.js
+++ script.js
@@ -1,7 +1,83 @@
 let gameSeq = [];
 let userSeq = [];
+let btns;
+let allBtns = document.querySelectorAll(".btn");
+let difficulty = document.querySelector("#difficulty");
+if(difficulty.options.selectedIndex == 0) {
+    btns = ["one", "two", "three", "four"];
+}
 
-let btns = ["one", "two", "three", "four"];
+
+difficulty.addEventListener("change",(event)=>{
+    if(event.target.value == "Easy") {
+        window.location.reload();
+    }
+
+
+    if(event.target.value == "Medium") {
+        btns = ["one", "two", "three", "four", "five", "six"];
+        let allBoxes = document.querySelector(".btn-container");
+        let lines = document.querySelectorAll(".line");
+        for(line of lines) {line.remove();}
+
+        let divArr = [];
+        for(let i = 0; i<btns.length; i++) {
+            let div = document.createElement("div");
+            div.className = `btn ${btns[i]}`;
+            div.id = `${btns[i]}`;
+            div.setAttribute("type", "button");
+            divArr.push(div);
+        }
+        // console.log(divArr)
+        for(let i=0; i<2; i++) {
+            let newLine = document.createElement("div");
+            newLine.className = "line";
+            newLine.appendChild(divArr.pop());
+            newLine.appendChild(divArr.pop());
+            newLine.appendChild(divArr.pop());
+            allBoxes.append(newLine);
+        }
+
+        allBtns = document.querySelectorAll(".btn");
+        for(btn of allBtns){
+            btn.style.height = "150px";
+            btn.style.width = "150px";
+            btn.addEventListener("click",btnPress);
+        }
+    }
+
+    if(event.target.value == "Hard") {
+        btns = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+        let allBoxes = document.querySelector(".btn-container");
+        let lines = document.querySelectorAll(".line");
+        for(line of lines) {line.remove();}
+
+        let divArr = [];
+        for(let i=0; i<btns.length; i++) {
+            let div = document.createElement("div");
+            div.className = `btn ${btns[i]}`;
+            div.id = `${btns[i]}`;
+            div.setAttribute("type", "button");
+            divArr.push(div);
+        }
+        for(let i=0; i<=2; i++) {
+            let newLine = document.createElement("div");
+            newLine.className = "line";
+            newLine.appendChild(divArr.pop());
+            newLine.appendChild(divArr.pop());
+            newLine.appendChild(divArr.pop());
+            allBoxes.append(newLine);
+        }
+
+        allBtns = document.querySelectorAll(".btn");
+        for(btn of allBtns){
+            btn.style.height = "125px";
+            btn.style.width = "125px";
+            btn.addEventListener("click",btnPress);
+        }
+    }
+});
+
 
 let highScore = document.querySelector("#highScore");
 
@@ -29,6 +105,13 @@ function levelUp() {
     level++;
     h3.innerText = `Level ${level}`;
     let randIdx = Math.floor(Math.random() * 4);
+    if (difficulty.options.selectedIndex == 1) {
+        randIdx = Math.floor(Math.random()* 6);
+    }
+    if (difficulty.options.selectedIndex == 2) {
+        randIdx = Math.floor(Math.random()*9);
+    }
+    console.log(randIdx);
     let randClass = btns[randIdx];
     gameSeq.push(randClass);
     let randBtn = document.querySelector(`.${randClass}`);
@@ -64,7 +147,6 @@ function btnPress() {
     checkAns(userSeq.length-1);
 }
 
-let allBtns = document.querySelectorAll(".btn");
 for(btn of allBtns){
     btn.addEventListener("click",btnPress);
 }
